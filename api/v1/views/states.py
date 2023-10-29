d32409063413275af754908cb4caba15b492880e
ghaidaareda <dodo.sa7afy@gmail.com>
2023-10-30 00:18:43 +0200

    pycode

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -7,6 +7,7 @@ from models.state import State
 from models import storage
 from api.v1.views import app_views
 
+
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_all_states():
     """get all states"""
@@ -15,25 +16,31 @@ def get_all_states():
         new_states.append(state.to_dict())
     return jsonify(new_states)
 
+
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def get_state(state_id):
     """ get state by id"""
-    state = storage.get(State ,state_id)
+    state = storage.get(State, state_id)
     if state:
         return jsonify(state.to_dict())
     else:
         abort(404)
-    
-@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+
+
+@app_views.route(
+    '/states/<state_id>',
+    methods=['DELETE'],
+    strict_slashes=False)
 def delete_state(state_id):
     """ get state by id"""
-    state = storage.get(State ,state_id)
+    state = storage.get(State, state_id)
     if state is None:
         abort(404)
     else:
         storage.delete(state)
         storage.save()
         return make_response(jsonify({}), 200)
+
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def post_state():
@@ -45,23 +52,21 @@ def post_state():
         abort(400, 'Missing name')
     state = State(**data)
     state.save()
-    
+
     return jsonify(state.to_dict()), 201
 
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def put_state(state_id):
     """update state"""
-    state = storage.get(State ,state_id)
+    state = storage.get(State, state_id)
     if state is None:
         abort(400)
     data = request.get_json()
     if 'name' not in data:
         abort(400, 'Not a JSON')
-    for key ,value in data:
+    for key, value in data:
         if key is not('id', 'created_at', 'updated_at'):
             setattr(state, key, value)
         state.save()
         return jsonify(state.to_dict()), 200
-    
-    
