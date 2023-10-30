d8dd7ab50f912908e8535c7174ca529c66d916ef
jailan-sh <jilan.elsherbiny@gmail.com>
2023-10-30 23:28:10 +0200

    push

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -7,14 +7,16 @@ from models import storage
 from api.v1.views import app_views
 
 
-@app_views.route('/amenities', methods=['GET'])
+@app_views.route('/amenities', methods=['GET'],
+                 strict_slashes=False)
 def get_amenities():
     """all amenities """
     amenity = [obj.to_dict() for obj in storage.all(Amenity).values()]
     return make_response(jsonify(amenity), 200)
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['GET'])
+@app_views.route('/amenities/<amenity_id>', methods=['GET'],
+                 strict_slashes=False)
 def get_amenity(amenity_id):
     """get one """
     amenity = storage.get(Amenity, amenity_id)
@@ -22,7 +24,8 @@ def get_amenity(amenity_id):
                          200) if amenity else abort(404)
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['DELETE'])
+@app_views.route('/amenities/<amenity_id>', methods=['DELETE'],
+                 strict_slashes=False)
 def delete_amenity(amenity_id):
     """remove amenity"""
     amenity = storage.get(Amenity, amenity_id)
@@ -33,7 +36,8 @@ def delete_amenity(amenity_id):
     return make_response({}, 200)
 
 
-@app_views.route('/amenities', methods=['POST'])
+@app_views.route('/amenities', methods=['POST'],
+                 strict_slashes=False)
 def create_amenity():
     """post amenity"""
     amenity = request.get_json()
@@ -46,7 +50,8 @@ def create_amenity():
     return make_response(jsonify(new_amenity.to_dict()), 201)
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['PUT'])
+@app_views.route('/amenities/<amenity_id>', methods=['PUT'],
+                 strict_slashes=False)
 def update_amenity(amenity_id):
     """update """
     amenity = storage.get(Amenity, amenity_id)
