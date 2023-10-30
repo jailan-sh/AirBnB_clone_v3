0060d032e80f98dd3fb206c4d8105acb9c9b581f
jailan-sh <jilan.elsherbiny@gmail.com>
2023-10-31 01:41:41 +0200

    end

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -12,24 +12,19 @@ from models import storage
 @app_views.route('/places/<place_id>/reviews', methods=['GET'],
                  strict_slashes=False)
 def get_reviews(place_id):
-    """
-    Retrieves the list of all Review objects of a Place
-    """
+    """Retrieves all Reviews of a Place"""
     place = storage.get(Place, place_id)
 
     if not place:
         abort(404)
 
     reviews = [review.to_dict() for review in place.reviews]
-
     return jsonify(reviews)
 
 
 @app_views.route('/reviews/<review_id>', methods=['GET'], strict_slashes=False)
 def get_review(review_id):
-    """
-    Retrieves a Review object
-    """
+    """Retrieves one"""
     review = storage.get(Review, review_id)
     if not review:
         abort(404)
@@ -40,9 +35,7 @@ def get_review(review_id):
 @app_views.route('/reviews/<review_id>', methods=['DELETE'],
                  strict_slashes=False)
 def delete_review(review_id):
-    """
-    Deletes a Review Object
-    """
+    """Delete"""
 
     review = storage.get(Review, review_id)
 
@@ -58,9 +51,7 @@ def delete_review(review_id):
 @app_views.route('/places/<place_id>/reviews', methods=['POST'],
                  strict_slashes=False)
 def post_review(place_id):
-    """
-    Creates a Review
-    """
+    """Creates a Review"""
     place = storage.get(Place, place_id)
 
     if not place:
@@ -89,9 +80,7 @@ def post_review(place_id):
 
 @app_views.route('/reviews/<review_id>', methods=['PUT'], strict_slashes=False)
 def put_review(review_id):
-    """
-    Updates a Review
-    """
+    """Updates """
     review = storage.get(Review, review_id)
 
     if not review:
